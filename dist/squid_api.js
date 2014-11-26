b153dea87d447b785ab330303e043151d756dd1c
obalbous <oba@squid>
2014-11-26 11:24:12 +0100

    facet cleanup and job reset

--- dist/squid_api.js
+++ dist/squid_api.js
@@ -871,15 +871,16 @@
 
         setProjectId : function(projectId) {
             this.set({"id": {
-                "projectId": projectId,
-                "analysisJobId": null
-            },
-            "domains": null,
-            "dimensions" : null,
-            "metrics" : null,
-            "selection" : null,
-            "results" : null}
-            );
+                    "projectId": projectId,
+                    "analysisJobId": null
+                },
+                "oid" : null,
+                "domains": null,
+                "dimensions" : null,
+                "metrics" : null,
+                "selection" : null,
+                "results" : null
+            });
             return this;
         },
 
@@ -897,12 +898,17 @@
             } else {
                 domains = null;
             }
-            this.set({"domains": domains,
-                "dimensions": null,
-                "metrics": null,
-                "selection": null,
-                "results" : null}
-            );
+            this.set({"id": {
+                    "projectId": this.get("id").projectId,
+                    "analysisJobId": null
+                },
+                "oid" : null,
+                "domains": domains,
+                "dimensions" : null,
+                "metrics" : null,
+                "selection" : null,
+                "results" : null
+            });
             return this;
         },
 
@@ -920,7 +926,19 @@
             } else {
                 dims = null;
             }
-            this.set("dimensions", dims);
+            this.setDimensions(dims);
+            return this;
+        },
+        
+        setDimensions : function(dimensions) {
+            this.set({"id": {
+                    "projectId": this.get("id").projectId,
+                    "analysisJobId": null
+                },
+                "oid" : null,
+                "dimensions" : dimensions,
+                "results" : null
+            });
             return this;
         },
 
@@ -932,7 +950,7 @@
                 "domainId": this.get("domains")[0].domainId,
                 "dimensionId": dimensionId
             };
-            this.set("dimensions", dims);
+            this.setDimensions(dims);
             return this;
         },
 
@@ -950,7 +968,19 @@
             } else {
                 metrics = null;
             }
-            this.set("metrics", metrics);
+            this.setMetrics(metrics);
+            return this;
+        },
+        
+        setMetrics : function(metrics) {
+            this.set({"id": {
+                    "projectId": this.get("id").projectId,
+                    "analysisJobId": null
+                },
+                "oid" : null,
+                "metrics" : metrics,
+                "results" : null
+            });
             return this;
         },
         
@@ -962,7 +992,7 @@
                 "domainId": this.get("domains")[0].domainId,
                 "metricId": metricId
             };
-            this.set("metrics", items);
+            this.setMetrics(items);
             return this;
         },
         
@@ -1114,6 +1144,17 @@
                     // use default filters
                     filters = squid_api.model.filters;
                     selection =  filters.get("selection");
+                    if (selection && selection.facets) {
+                        // cleanup off the facets
+                        for (var i=0; i<selection.facets.length; i++) {
+                            var facet = selection.facets[i];
+                            // remove items
+                            delete facet.items;
+                            delete facet.totalSize;
+                            // cleanup dimensions
+                            facet.dimension = {"id":facet.dimension.id};
+                        }
+                    }
                 }
             } else {
                 selection =  filters.get("selection");
--- src/squid_api_analysisjob_controller.js
+++ src/squid_api_analysisjob_controller.js
@@ -45,15 +45,16 @@
 
         setProjectId : function(projectId) {
             this.set({"id": {
-                "projectId": projectId,
-                "analysisJobId": null
-            },
-            "domains": null,
-            "dimensions" : null,
-            "metrics" : null,
-            "selection" : null,
-            "results" : null}
-            );
+                    "projectId": projectId,
+                    "analysisJobId": null
+                },
+                "oid" : null,
+                "domains": null,
+                "dimensions" : null,
+                "metrics" : null,
+                "selection" : null,
+                "results" : null
+            });
             return this;
         },
 
@@ -71,12 +72,17 @@
             } else {
                 domains = null;
             }
-            this.set({"domains": domains,
-                "dimensions": null,
-                "metrics": null,
-                "selection": null,
-                "results" : null}
-            );
+            this.set({"id": {
+                    "projectId": this.get("id").projectId,
+                    "analysisJobId": null
+                },
+                "oid" : null,
+                "domains": domains,
+                "dimensions" : null,
+                "metrics" : null,
+                "selection" : null,
+                "results" : null
+            });
             return this;
         },
 
@@ -94,7 +100,19 @@
             } else {
                 dims = null;
             }
-            this.set("dimensions", dims);
+            this.setDimensions(dims);
+            return this;
+        },
+        
+        setDimensions : function(dimensions) {
+            this.set({"id": {
+                    "projectId": this.get("id").projectId,
+                    "analysisJobId": null
+                },
+                "oid" : null,
+                "dimensions" : dimensions,
+                "results" : null
+            });
             return this;
         },
 
@@ -106,7 +124,7 @@
                 "domainId": this.get("domains")[0].domainId,
                 "dimensionId": dimensionId
             };
-            this.set("dimensions", dims);
+            this.setDimensions(dims);
             return this;
         },
 
@@ -124,7 +142,19 @@
             } else {
                 metrics = null;
             }
-            this.set("metrics", metrics);
+            this.setMetrics(metrics);
+            return this;
+        },
+        
+        setMetrics : function(metrics) {
+            this.set({"id": {
+                    "projectId": this.get("id").projectId,
+                    "analysisJobId": null
+                },
+                "oid" : null,
+                "metrics" : metrics,
+                "results" : null
+            });
             return this;
         },
         
@@ -136,7 +166,7 @@
                 "domainId": this.get("domains")[0].domainId,
                 "metricId": metricId
             };
-            this.set("metrics", items);
+            this.setMetrics(items);
             return this;
         },
         
@@ -288,6 +318,17 @@
                     // use default filters
                     filters = squid_api.model.filters;
                     selection =  filters.get("selection");
+                    if (selection && selection.facets) {
+                        // cleanup off the facets
+                        for (var i=0; i<selection.facets.length; i++) {
+                            var facet = selection.facets[i];
+                            // remove items
+                            delete facet.items;
+                            delete facet.totalSize;
+                            // cleanup dimensions
+                            facet.dimension = {"id":facet.dimension.id};
+                        }
+                    }
                 }
             } else {
                 selection =  filters.get("selection");
