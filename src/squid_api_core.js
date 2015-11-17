27304b340c2bbc96495a9fdf8f0c731db9482836
olivier <obalbous@squidsolutions.com>
2015-11-17 14:06:49 +0100

    T632 moved backbone forms definitions to squid_api_admin

--- src/squid_api_core.js
+++ src/squid_api_core.js
@@ -1275,10 +1275,7 @@
     squid_api.model.ProjectModel = squid_api.model.BaseModel.extend({
         urlRoot: function() {
             return this.baseRoot() + "/projects/" + (this.get("id").projectId || "");
-        },
-        definition : "Project",
-        ignoredAttributes : ['accessRights', 'config', 'relations', 'domains'],
-        schema : {"id":{"title":" ","type":"Object","subSchema":{"projectId":{"options":[],"type":"Text","editorClass":"hidden"}},"editorClass":"hidden","fieldClass":"id"},"name":{"type":"Text","editorClass":"form-control","fieldClass":"name"},"dbUrl":{"title":"Database URL","type":"Text","editorClass":"form-control","position":1,"fieldClass":"dbUrl"},"dbUser":{"title":"Database User","type":"Text","editorClass":"form-control","position":2,"fieldClass":"dbUser"},"dbPassword":{"title":"Database Password","type":"Password","editorClass":"form-control","position":3,"fieldClass":"dbPassword"},"dbSchemas":{"title":"Database Schemas","type":"Checkboxes","editorClass":" ","options":[],"position":4,"fieldClass":"dbSchemas"}}
+        }
     });
 
     squid_api.model.ProjectCollection = squid_api.model.BaseCollection.extend({
@@ -1310,10 +1307,7 @@
     squid_api.model.DomainModel = squid_api.model.ProjectModel.extend({
         urlRoot: function() {
             return squid_api.model.ProjectModel.prototype.urlRoot.apply(this, arguments) + "/domains/" + (this.get("id").domainId || "");
-        },
-        definition : "Domain",
-        ignoredAttributes : ['accessRights', 'dimensions', 'metrics'],
-        schema : {"id":{"title":" ","type":"Object","subSchema":{"projectId":{"options":[],"type":"Text","editorClass":"hidden"},"domainId":{"options":[],"type":"Text","editorClass":"form-control"}},"editorClass":"hidden","fieldClass":"id"},"name":{"type":"Text","editorClass":"form-control","fieldClass":"name"},"subject":{"type":"Object","title":"","subSchema":{"value":{"title":"Subject Value","type":"TextArea","editorClass":"form-control suggestion-box"}},"position":1,"fieldClass":"subject"}}
+        }
     });
 
     squid_api.model.DomainCollection = squid_api.model.BaseCollection.extend({
@@ -1326,10 +1320,7 @@
     squid_api.model.RelationModel = squid_api.model.ProjectModel.extend({
         urlRoot: function() {
             return squid_api.model.ProjectModel.prototype.urlRoot.apply(this, arguments) + "/relations/" + this.get("id").relationId;
-        },
-        definition: "Relation",
-        ignoredAttributes : ['accessRights'],
-        schema : {"id":{"title":" ","type":"Object","subSchema":{"projectId":{"options":[],"type":"Text","title":" ","editorClass":"hidden"},"relationId":{"options":[],"type":"Text","editorClass":"form-control"}},"editorClass":"hidden","fieldClass":"id"},"leftId":{"title":" ","type":"Object","subSchema":{"projectId":{"options":[],"type":"Text","title":" ","editorClass":"hidden"},"domainId":{"options":[],"type":"Select","editorClass":"form-control","title":"Left Domain"}},"fieldClass":"leftId"},"leftCardinality":{"type":"Select","editorClass":"form-control","options":["ZERO_OR_ONE","ONE","MANY"],"fieldClass":"leftCardinality"},"rightCardinality":{"type":"Select","editorClass":"form-control","options":["ZERO_OR_ONE","ONE","MANY"],"fieldClass":"rightCardinality"},"rightId":{"title":" ","type":"Object","subSchema":{"projectId":{"options":[],"type":"Text","title":" ","editorClass":"hidden"},"domainId":{"options":[],"type":"Select","editorClass":"form-control","title":"Right Domain"}},"fieldClass":"rightId"},"leftName":{"type":"Text","editorClass":"form-control","fieldClass":"leftName"},"rightName":{"type":"Text","editorClass":"form-control","fieldClass":"rightName"},"joinExpression":{"title":" ","type":"Object","subSchema":{"value":{"title":"Join Expression","type":"TextArea","editorClass":"form-control suggestion-box"}},"fieldClass":"joinExpression"}}
+        }
     });
 
     squid_api.model.RelationCollection = squid_api.model.BaseCollection.extend({
@@ -1342,10 +1333,7 @@
     squid_api.model.DimensionModel = squid_api.model.DomainModel.extend({
         urlRoot: function() {
             return squid_api.model.DomainModel.prototype.urlRoot.apply(this, arguments) + "/dimensions/" + (this.get("id").dimensionId || "");
-        },
-        definition: "Dimension",
-        ignoredAttributes : ['options', 'accessRights', 'dynamic', 'attributes', 'valueType'],
-        schema : {"id":{"title":" ","type":"Object","subSchema":{"projectId":{"options":[],"type":"Text","editorClass":"hidden"},"domainId":{"options":[],"type":"Text","editorClass":"form-control"},"dimensionId":{"options":[],"type":"Text","editorClass":"form-control"}},"editorClass":"hidden","fieldClass":"id"},"name":{"type":"Text","editorClass":"form-control","fieldClass":"name"},"type":{"type":"Checkboxes","editorClass":" ","options":[{"val":"CATEGORICAL","label":"Indexed"},{"val":"CONTINUOUS","label":"Period"}],"position":1,"fieldClass":"type"},"parentId":{"title":" ","type":"Object","subSchema":{"projectId":{"options":[],"type":"Text","editorClass":"hidden","fieldClass":"hidden"},"domainId":{"options":[],"type":"Text","editorClass":"form-control","fieldClass":"hidden"},"dimensionId":{"options":[],"type":"Text","editorClass":"form-control","title":"Parent Dimension"}},"position":2,"fieldClass":"parentId"},"expression":{"type":"Object",title:"","subSchema":{"value":{"type":"TextArea","editorClass":"form-control suggestion-box","title":"Expression Value"}},"position":3,"fieldClass":"expression"}}
+        }
     });
 
     squid_api.model.DimensionCollection = squid_api.model.BaseCollection.extend({
@@ -1358,9 +1346,7 @@
     squid_api.model.MetricModel = squid_api.model.DomainModel.extend({
         urlRoot: function() {
             return squid_api.model.DomainModel.prototype.urlRoot.apply(this, arguments) + "/metrics/" + (this.get("id").metricId || "");
-        },
-        definition: "Metric",
-        schema : {"id":{"title":" ","type":"Object","subSchema":{"projectId":{"options":[],"type":"Text","editorClass":"hidden"},"domainId":{"options":[],"type":"Text","editorClass":"form-control"},"metricId":{"options":[],"type":"Text","editorClass":"form-control"}},"editorClass":"hidden","fieldClass":"id"},"dynamic":{"type":"Text","editorClass":"form-control","fieldClass":"dynamic hidden"},"name":{"type":"Text","editorClass":"form-control","fieldClass":"name"},"expression":{"title":"","type":"Object","subSchema":{"value":{"title":"Expression Value","type":"TextArea","editorClass":"form-control suggestion-box"}},"position":1,"fieldClass":"expression"}}
+        }
     });
 
     squid_api.model.MetricCollection = squid_api.model.BaseCollection.extend({
@@ -1373,56 +1359,6 @@
     squid_api.model.BookmarkModel = squid_api.model.ProjectModel.extend({
         urlRoot: function() {
             return squid_api.model.ProjectModel.prototype.urlRoot.apply(this, arguments) + "/bookmarks/" + (this.get("id").bookmarkId || "");
-        },
-        definition : "Bookmark",
-        ignoredAttributes : ['accessRights'],
-        schema : {
-            "id" : {
-                "title" : " ",
-                "type" : "Object",
-                "subSchema" : {
-                    "projectId" : {
-                        "options" : [],
-                        "type" : "Text",
-                        "editorClass" : "hidden"
-                    },
-                    "bookmarkId" : {
-                        "options" : [],
-                        "type" : "Text",
-                        "editorClass" : "form-control"
-                    }
-                },
-                "editorClass" : "hidden",
-                "fieldClass" : "id"
-            },
-            "name" : {
-                "type" : "Text",
-                "editorClass" : "form-control",
-                "fieldClass" : "name"
-            },
-            "description" : {
-                "type" : "Text",
-                "editorClass" : "form-control",
-                "fieldClass" : "description"
-            },
-            "path" : {
-                "type" : "Text",
-                "editorClass" : "form-control",
-                "fieldClass" : "path"
-            },
-            "config" : {
-                "type" : "Object",
-                "title" : "",
-                "subSchema" : {
-                    "value" : {
-                        "title" : "Value",
-                        "type" : "TextArea",
-                        "editorClass" : "form-control suggestion-box"
-                    }
-                },
-                "position" : 1,
-                "fieldClass" : "config"
-            }
         }
     });
 
