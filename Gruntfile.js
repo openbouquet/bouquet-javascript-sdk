7dc8b2835dcc8a3ae59d95dea5afd90ee02393ee
olivier <obalbous@squidsolutions.com>
2016-01-26 14:40:39 +0100

    dev should be default for watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
         watch : {
             js : {
                 files : [ 'src/**/*.*' ],
-                tasks : [ 'default' ]
+                tasks : [ 'dev' ]
             }
         }
     });
